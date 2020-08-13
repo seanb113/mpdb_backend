a7f0c9e332afef1c58647aac92b129d81db501fb
Sean Beach <sean.beach1@yahoo.com>
2020-08-12 21:58:13 -0400

    migrations

--- db/migrate/20200716012235_create_interesteds.rb
+++ db/migrate/20200716012235_create_interesteds.rb
@@ -2,7 +2,7 @@ class CreateInteresteds < ActiveRecord::Migration[6.0]
   def change
     create_table :interesteds do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :pitch, null: false, foreign_key: tru
+      t.references :pitch, null: false, foreign_key: true
       t.timestamps
     end
   end
